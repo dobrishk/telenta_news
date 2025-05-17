78f47a147ddc744cff17df3bd5379f32a388f48c
dobrishk <132073097+dobrishk@users.noreply.github.com>
2025-05-17 13:32:44 +0700

    Update Censor.py

--- Censor.py
+++ Censor.py
@@ -1,16 +1,19 @@
-blacklist = {'popka' : 'mamonta'}
-def ret():
-    return blacklist['popka']
+blacklist = {}
+
 
 def new_check(message, user, last_messages):
-    _blacklist = blacklist[user]
-    _last_messages = last_messages[user]
-    if message.intersection(_blacklist):
+    _blacklist = set(blacklist.get(user, []))
+    _last_messages = last_messages.get(user, [])
+
+    # Проверка по чёрному списку
+    message_words = set(message.lower().split())
+    if message_words & _blacklist:
         return False
-    for words in _last_messages:
-        length = len(words)
-        common_len = len(words.intersection(message))
-        percent = (common_len / length) * 100
-        if percent >= 40:
+
+    # Проверка на дубликаты (40% совпадений)
+    for stored_msg, _ in _last_messages:
+        stored_words = set(stored_msg.lower().split())
+        common = len(stored_words & message_words)
+        if (common / len(stored_words)) * 100 >= 40:
             return False
     return True
